0c6901afbbc5fea3ff73c2c465523d5f77544953
Minh Nguyen <mnguyen728@hotmail.com>
2021-09-11 05:49:15 -0700

    init files

---
+++ server.js
@@ -0,0 +1,38 @@
+// Import dependencies 
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require("morgan");
+
+// Import routes
+const routes = require('./controllers')
+
+// Sets up express 
+const PORT = process.env.PORT || 3002;
+const app = express()
+
+// Sets up morgan logs
+app.use(logger("dev"));
+
+// Sets up data parsing 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files and use routes
+app.use(express.static("public"));
+app.use(routes)
+
+// Connects to mongoose database
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
